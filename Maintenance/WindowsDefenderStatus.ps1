2d357f62872193aaeb06f658a29311267ee21db5
Niklas Zistler <niklas.zistler@rit.de>
2021-10-27 14:54:38 +0200

    Changes for Windows Server 2012

--- Maintenance/WindowsDefenderStatus.ps1
+++ Maintenance/WindowsDefenderStatus.ps1
@@ -14,6 +14,7 @@ $AVSignatureVersionLastUpdate
 $AVSignatureLastUpdateInDays
 $AntiMalewareClientVersion
 $QuarantinedItems
+$OperatingSystem
 
 #Script
 
@@ -22,77 +23,85 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     {
         Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit
     }
+#Check if the operating System is 2016 or newer
+$OperatingSystem = (Systeminfo | Select-String 'Betriebssystemname:')
+$OperatingSystem = $OperatingSystem.ToString().Split(':')[1].Trim()
+
+if($OperatingSystem -notmatch "2012"){
+
+    ######Configuration Status of the Windows Defender
+    #Scheduale Scan Day
+    $DefenderScheduleDayStatus = (Get-MpPreference).ScanScheduleDay
+    switch ($DefenderScheduleDayStatus) {
+        0 {$DefenderScheduleDay = "Täglich"}
+        1 {$DefenderScheduleDay = "Sonntag"}
+        2 {$DefenderScheduleDay = "Montag"}
+        4 {$DefenderScheduleDay = "Dienstag"}
+        5 {$DefenderScheduleDay = "Mittwoch"}
+        6 {$DefenderScheduleDay = "Donnerstag"}
+        7 {$DefenderScheduleDay = "Freitag"}
+        8 {$DefenderScheduleDay = "Samstag"}
+        9 {$DefenderScheduleDay = "Nie"}
+        Default {$DefenderScheduleDayStatus = ""}
+    }
+
+    #Status Netzwerkscan
+    $NetworkScanStatus = (Get-MpPreference).EnableNetworkProtection
+    switch ($NetworkScanStatus){
+        0 {$NetworkScan = "Ja"}
+        1 {$NetworkScan = "Nein"}
+        2 {$NetworkScan = "Überwachung"}
+        Default {$NetworkScan = ""}
+    }
+    if (($NetworkScan -eq "aus") -and ((Get-MpPreference).DisableScanningMappedNetworkDrivesForFullScan -eq "True")){
+        $NetworkScan = "Aus"
+    }
+    elseif(($NetworkScan -eq "aus") -and ((Get-MpPreference).DisableScanningMappedNetworkDrivesForFullScan -eq "False")){
+        Write-Host -ForegroundColor Red "Die Netzwerk Protection ist aus und das Scannen von Netzlaufwerken ist eingeschaltet!"
+    }
+    elseif(($NetworkScan -eq "ein") -and ((Get-MpPreference).DisableScanningMappedNetworkDrivesForFullScan -eq "True")){
+        Write-Host -ForegroundColor Red "Die Netzwerk Protection ist eingeschalten und das Scannen von Netzlaufwerken ist aus!"
+    }
+    elseif(($NetworkScan -eq "ein") -and ((Get-MpPreference).DisableScanningMappedNetworkDrivesForFullScan -eq "False")){
+        Write-Host -ForegroundColor Red "Die Netzwerk Protection und das Scannen von Netzlaufwerken ist eingeschaltet!"
+    }
 
 
-######Configuration Status of the Windows Defender
-#Scheduale Scan Day
-$DefenderScheduleDayStatus = (Get-MpPreference).ScanScheduleDay
-switch ($DefenderScheduleDayStatus) {
-    0 {$DefenderScheduleDay = "Täglich"}
-    1 {$DefenderScheduleDay = "Sonntag"}
-    2 {$DefenderScheduleDay = "Montag"}
-    4 {$DefenderScheduleDay = "Dienstag"}
-    5 {$DefenderScheduleDay = "Mittwoch"}
-    6 {$DefenderScheduleDay = "Donnerstag"}
-    7 {$DefenderScheduleDay = "Freitag"}
-    8 {$DefenderScheduleDay = "Samstag"}
-    9 {$DefenderScheduleDay = "Nie"}
-    Default {$DefenderScheduleDayStatus = ""}
-}
+    #######Information about signature versions, last update, last scan, and more.
+    ### AV Status
+    if((Get-MpComputerStatus).AMServiceEnabled -eq "True" -and (Get-MpComputerStatus).AntispywareEnabled -eq "True" -and (Get-MpComputerStatus).BehaviorMonitorEnabled -eq "True"-and
+            (Get-MpComputerStatus).AntivirusEnabled -eq "True" -and (Get-MpComputerStatus).OnAccessProtectionEnabled -eq "True" -and (Get-MpComputerStatus).RealTimeProtectionEnabled -eq"True"){
+        $AVStatus = "Ja"
+    }
+    else{
+        Write-Host -ForegroundColor Red "Mindestens eines der relevanten Module ist aus!"
+        $AVStatus = "Nein"
+    }
 
-#Status Netzwerkscan
-$NetworkScanStatus = (Get-MpPreference).EnableNetworkProtection
-switch ($NetworkScanStatus){
-    0 {$NetworkScan = "Ja"}
-    1 {$NetworkScan = "Nein"}
-    2 {$NetworkScan = "Überwachung"}
-    Default {$NetworkScan = ""}
-}
-if (($NetworkScan -eq "aus") -and ((Get-MpPreference).DisableScanningMappedNetworkDrivesForFullScan -eq "True")){
-    $NetworkScan = "Aus"
-}
-elseif(($NetworkScan -eq "aus") -and ((Get-MpPreference).DisableScanningMappedNetworkDrivesForFullScan -eq "False")){
-    Write-Host -ForegroundColor Red "Die Netzwerk Protection ist aus und das Scannen von Netzlaufwerken ist eingeschaltet!"
-}
-elseif(($NetworkScan -eq "ein") -and ((Get-MpPreference).DisableScanningMappedNetworkDrivesForFullScan -eq "True")){
-    Write-Host -ForegroundColor Red "Die Netzwerk Protection ist eingeschalten und das Scannen von Netzlaufwerken ist aus!"
-}
-elseif(($NetworkScan -eq "ein") -and ((Get-MpPreference).DisableScanningMappedNetworkDrivesForFullScan -eq "False")){
-    Write-Host -ForegroundColor Red "Die Netzwerk Protection und das Scannen von Netzlaufwerken ist eingeschaltet!"
-}
+    ### Signatur Version und Alter
+    $AVSignatureVersion =  (Get-MpComputerStatus).AntivirusSignatureVersion
+    $AVSignatureVersionLastUpdate = (Get-MpComputerStatus).AntispywareSignatureLastUpdated
+    $AVSignatureLastUpdateInDays = (Get-MpComputerStatus).AntivirusSignatureAge
+    $AntiMalewareClientVersion = (Get-MpComputerStatus).AMProductVersion
+
+    ### Quarantaene
+    $QuarantinedItems = ( Get-ChildItem "C:\ProgramData\Microsoft\Windows Defender\Quarantine" | Measure-Object ).Count + ( Get-ChildItem "C:\ProgramData\Microsoft\Windows Defender\LocalCopy" | Measure-Object ).Count
+
+    ####Ausgabe
+    Write-Host -ForegroundColor Yellow "Status des Windows Defenders"
+    Write-Host -ForegroundColor Green "Virenschutz ein? :" $AVStatus
+    Write-Host -ForegroundColor Green "Netzwerkbedrohungsschutz aus? :" $NetworkScan
+    Write-Host -ForegroundColor Green "Versionsstand der Software: " $AntiMalewareClientVersion
+    Write-Host -ForegroundColor Green "Letztes Definitionsupdate: " $AVSignatureVersionLastUpdate
+    Write-Host -ForegroundColor Green "Alter der Definitionen in Tagen: " $AVSignatureLastUpdateInDays
+    Write-Host -ForegroundColor Green "Anzahl der Elemente in der Quarantäne: " $QuarantinedItems
+    Write-Host -ForegroundColor Green "Häufigkeit geplanter Scans: " $DefenderScheduleDay
 
 
-#######Information about signature versions, last update, last scan, and more.
-### AV Status
-if((Get-MpComputerStatus).AMServiceEnabled -eq "True" -and (Get-MpComputerStatus).AntispywareEnabled -eq "True" -and (Get-MpComputerStatus).BehaviorMonitorEnabled -eq "True"-and
-        (Get-MpComputerStatus).AntivirusEnabled -eq "True" -and (Get-MpComputerStatus).OnAccessProtectionEnabled -eq "True" -and (Get-MpComputerStatus).RealTimeProtectionEnabled -eq"True"){
-    $AVStatus = "Ja"
+    ####Skript Ende
+    Write-Host -NoNewLine "Beliebige Taste zum Beenden druecken";
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
 }
 else{
-    Write-Host -ForegroundColor Red "Mindestens eines der relevanten Module ist aus!"
-    $AVStatus = "Nein"
+    Write-Host -ForegroundColor Yellow "Das Skritp muss auf einem Host mit dem Betriebssystem Windows Server 2016 oder neuer ausgeführt werden."
 }
-
-### Signatur Version und Alter
-$AVSignatureVersion =  (Get-MpComputerStatus).AntivirusSignatureVersion
-$AVSignatureVersionLastUpdate = (Get-MpComputerStatus).AntispywareSignatureLastUpdated
-$AVSignatureLastUpdateInDays = (Get-MpComputerStatus).AntivirusSignatureAge
-$AntiMalewareClientVersion = (Get-MpComputerStatus).AMProductVersion
-
-### Quarantaene
-$QuarantinedItems = ( Get-ChildItem "C:\ProgramData\Microsoft\Windows Defender\Quarantine" | Measure-Object ).Count + ( Get-ChildItem "C:\ProgramData\Microsoft\Windows Defender\LocalCopy" | Measure-Object ).Count
-
-####Ausgabe
-Write-Host -ForegroundColor Yellow "Status des Windows Defenders"
-Write-Host -ForegroundColor Green "Virenschutz ein? :" $AVStatus
-Write-Host -ForegroundColor Green "Netzwerkbedrohungsschutz aus? :" $NetworkScan
-Write-Host -ForegroundColor Green "Versionsstand der Software: " $AntiMalewareClientVersion
-Write-Host -ForegroundColor Green "Letztes Definitionsupdate: " $AVSignatureVersionLastUpdate
-Write-Host -ForegroundColor Green "Alter der Definitionen in Tagen: " $AVSignatureLastUpdateInDays
-Write-Host -ForegroundColor Green "Anzahl der Elemente in der Quarantäne: " $QuarantinedItems
-Write-Host -ForegroundColor Green "Häufigkeit geplanter Scans: " $DefenderScheduleDay
-
-
-####Skript Ende
-Write-Host -NoNewLine "Beliebige Taste zum Beenden druecken";
-$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
